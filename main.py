3dfb89259c6c10bbfa7e1bd244e5476a670886f4
Siddharth <siddharth8462siddharth@gmail.com>
2021-11-14 16:27:19 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+print("Hello World", end=" .") #this is to end print statement without new line
+# I am printing my name.
+"""
+This is 
+a 
+multi line
+comment
+"""
+print("My name is Siddharth Vats")
+print("Using \n \t escape sequence characters")
---
+++ tut7.py
@@ -0,0 +1,24 @@
+# var1 = "hello world"
+# var2 = 4
+# var3 = 36.7
+# var4 = "Siddharth"
+# var5 = "52"
+# var6 = "34"
+# print(type(var1))
+# print(var2+var3)
+# print(var1 + " " + var4 ) # we can use + sign to add same types of data types 
+# print(100* str(int(var5) + int(var6))) #type casting in python
+# print(10 * "Hello World\n") #crazyness of python
+
+
+# print("Enter your number")
+# varinp = input()
+# print("You entered" , int(varinp) + 10)
+
+
+# Quick quiz
+print("Enter first number")
+firstNum = int(input())
+print("Enter second number")
+secondNum = int(input())
+print("The sum is", firstNum+secondNum)
