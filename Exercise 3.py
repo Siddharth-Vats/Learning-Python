cce86ed7fa71454290a6b3a71e70fa8390ca70a1
Siddharth <siddharth8462siddharth@gmail.com>
2021-12-02 22:06:55 +0530

    Learnt operators and short hand if else

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,6 +1,24 @@
-n = 18
 
-# no. of gusses = 9
-# print no. of gusses left
-# no. of gusses he took to finish
+
+# no. of guesses = 9
+# print no. of guesses left
+# no. of guesses he took to finish
 # print game over
+
+n = 45
+i = 0
+chance = 0
+while i <= 9:
+    inp = int(input("Enter a number :- "))
+    if inp == n:
+        print("Congrats you won in ", chance + 1, "chances")
+        break
+    elif inp < n:
+        print("Lesser!! try again and chances left is -", 8 - chance)
+        chance = chance + 1
+    else:
+        print("Greater!! try again and chances left is -", 8 - chance)
+        chance = chance + 1
+    if chance == 9:
+        print("GAME OVER!!")
+        break
---
+++ tut21.py
@@ -0,0 +1,54 @@
+# OPERATORS IN PYTHON
+# Arithmetic Operators
+# Assignment Operators
+# Comparison Operators
+# Logical Operators
+# Identity Operators
+# Membership Operators
+# Bitwise Operators
+
+# Arithmetic Operators
+# print("5 + 6 is", 5+6)
+# print("5 - 6 is", 5-6)
+# print("5 * 6 is", 5*6)
+# print("5 / 6 is", 5/6)
+# print("5 ** 6 is", 5**6)  # exponent operator
+# print("15 % 6 is", 15%6)
+# print("15 // 6 is", 15//6)  # returns integer value
+
+# Assignment Operators
+# x = 5
+# print(x)
+# x += 7
+# print(x)
+
+# Comparision Operators
+# i = 8
+# print(i==8)
+# print(i>=8)
+# print(i<=8)
+# print(i!=8)
+
+# Logical Operators
+# a = True
+# b = False
+# print(a and a)
+# print(a and b)
+# print(a or a)
+# print(a or b)
+    
+# Identity Operators
+# a = True
+# b = False
+# print(a is b)
+# print(a is not b)
+
+# Membership Operators
+# list = [3,34,32,56,87,64,54,6,45,65,6,546,45,6,564,6,45,6,54,65,6]
+# print(34 in list)
+# print(3245 not in list)
+
+# Bitwise Operators
+print(0 & 1)  # & and sign, | or sign
+print(0 | 1)
+
