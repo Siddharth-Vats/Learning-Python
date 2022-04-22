e6ffd9fee43d4f5e2fce44fa0b2c1485ae3de8e0
Siddharth <siddharth8462siddharth@gmail.com>
2022-04-22 22:10:47 +0530

    Learnt join function

---
+++ JOINFUNC.py
@@ -0,0 +1,9 @@
+lis = ["siddharth", "shreyas", "parth", "francisco", "tommy", "jerry"]
+
+# for item in lis:
+#     print(f"{item} and" , end=" ")
+
+a = " and ".join(lis)
+print(a)
+
+print("and more people in the world")
---
+++ tutmain1.py
@@ -0,0 +1,15 @@
+
+
+
+def printsid(string):
+    return f"yeah string siddharth ko de de thakur {string}"
+
+def add(num1, num2):
+    return num1+num2+5
+
+print("and the __name__ is", __name__)
+
+if __name__ == '__main__':
+    print(printsid("Siddharth"))    
+    o = add(4, 9)
+    print(o)
